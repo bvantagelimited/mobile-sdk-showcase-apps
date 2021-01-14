a319949fdc8a45ca87f6d476fb3b2f6ec7ab7f48
tinhnguyen <tn@ipification>
2021-01-14 13:32:39 +0700

    remove old RN project

--- ipification-sdk-react-native-js/App.js
+++ ipification-sdk-react-native-js/App.js
@@ -8,81 +8,83 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View, NativeModules, Button} from 'react-native';
 const {RNCoverageService, RNAuthenticationService} = NativeModules;
+import {Platform} from 'react-native';
+
 export default class App extends Component {
  constructor(props) {
    super(props);
    this.state = {
      activated: '',
-     authentication:''
+     authentication: '',
    };
-  
  }
  componentWillUnmount = () => {
-  if (Platform.OS === 'android') {
-    console.log("unregisterNetwork")
-    RNCoverageService.unregisterNetwork();
-  }
-};
-  checkCoverage = () => {
-   console.log("checkCoverage")
-   RNCoverageService.checkCoverage((err, success) => {
-     console.log(" success ", err, success)
-     if(success != null){
-       this.setState({activated: success});
+   if (Platform.OS === 'android') {
+     console.log("componentWillUnmount android")
+     RNCoverageService.unregisterNetwork();
+   }
+ };
+ checkCoverage = () => {
+   console.log('checkCoverage');
+   RNCoverageService.setAuthorizationServiceConfiguration("ipification-services.json")
+   RNCoverageService.checkCoverage(
+     (error, isAvailable) => {
+       console.log(' isAvailable ',isAvailable,  error);
+       if (isAvailable) {
+         this.setState({activated: isAvailable});
+       } else {
+         this.setState({activated: error});
+       }
      }
-     else if(err != null){
-       this.setState({activated: err});
-     }
-    
-   }, (err, failed) => {
-     console.log(" failed ", failed)
-     if(failed != null){
-       this.setState({activated: failed});
-     }
-     else if(err != null){
-       this.setState({activated: err});
-     }
-   });
+   );
  };
 
  doAuthentication = () => {
-   console.log("doAuthentication")
-   RNAuthenticationService.doAuthorizationWithParams({login_hint: "85263480857"}, (error, success) => {
-     console.log(error, success)
-     if(success != null){
-       this.setState({authentication: success});
+   console.log('doAuthentication');
+   RNAuthenticationService.doAuthorization(
+     {login_hint: '85263480857', state: '0829332e-3c3f-46ff-8de3-221c'},
+     (error, code) => {
+       console.log(error, code);
+       if (code != null) {
+         this.setState({authentication: code});
+       }
+       else{
+         this.setState({authentication: error});
+       }
      }
-     if(error != null){
-       this.setState({authentication: error});
-     }
-    
-   }, (error, failed) => {
-     console.log(error, failed)
-     if(failed != null){
-       this.setState({authentication: failed});
-     }
-     if(error != null){
-       this.setState({authentication: error});
-     }
-   });
+   );
  };
  render() {
    return (
      <View style={styles.container}>
        <Text style={{fontSize: 24}}>
-       CoverageApi Result: {`${this.state.activated}`}
+         CoverageApi Result: {`${this.state.activated}`}
        </Text>
        <Text style={{fontSize: 24, margin: 10}}>
-         DoAuthentcation Result: <Text style={{fontSize: 11}}> {`${this.state.authentication}`}</Text>
+         DoAuthentcation Result:{' '}
+         <Text style={{fontSize: 11}}> {`${this.state.authentication}`}</Text>
        </Text>
-       <View style={{height:30}}/>
-       <Button onPress={this.checkCoverage} title="Check Coverage " color="green" />
-       <View style={{height:10}}/>
-       <Button onPress={this.doAuthentication} title="Do Authentication" color="blue" />
+       <View style={{height: 30}} />
+       <Button
+         onPress={this.checkCoverage}
+         title="Check Coverage "
+         color="green"
+       />
+       <View style={{height: 10}} />
+       <Button
+         onPress={this.doAuthentication}
+         title="Do Authentication"
+         color="blue"
+       />
      </View>
    );
  }
 }
 const styles = StyleSheet.create({
- container: {flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#d3d3d3',},
+ container: {
+   flex: 1,
+   justifyContent: 'center',
+   alignItems: 'center',
+   backgroundColor: '#d3d3d3',
+ },
 });
--- ipification-sdk-react-native-js/ios/RNCoverageService.m
+++
@@ -1,6 +0,0 @@
-#import <Foundation/Foundation.h>
-#import "React/RCTBridgeModule.h"
-
-@interface RCT_EXTERN_MODULE(RNCoverageService,NSObject)
-RCT_EXTERN_METHOD(checkCoverage: (RCTResponseSenderBlock)callbackSuccess failed: (RCTResponseSenderBlock)callbackError)
-@end
