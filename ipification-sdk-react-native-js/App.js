97a89011533145728131c693fec94648662a6332
tinhnguyen <tn@ipification>
2021-01-14 17:44:23 +0700

    update flow for RN

--- ipification-sdk-react-native-js/App.js
+++ ipification-sdk-react-native-js/App.js
@@ -5,25 +5,46 @@
 * @format
 * @flow strict-local
 */
-import React, {Component} from 'react';
-import {StyleSheet, Text, View, NativeModules, Button} from 'react-native';
+import React, {useState, useRef, useEffect} from 'react';
+
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  StatusBar,
+  TouchableOpacity,
+  Text,
+  NativeModules
+} from 'react-native';
 const {RNCoverageService, RNAuthenticationService} = NativeModules;
 import {Platform} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import PhoneInput from 'react-native-phone-number-input';
 
-export default class App extends Component {
- constructor(props) {
-   super(props);
-   this.state = {
-     activated: '',
-     authentication: '',
-   };
- }
- componentWillUnmount = () => {
-   if (Platform.OS === 'android') {
-     console.log("componentWillUnmount android")
-     RNCoverageService.unregisterNetwork();
+import jwt_decode from "jwt-decode";
+
+const App = () => {
+  const TOKEN_URL = "https://stage.ipification.com/auth/realms/ipification/protocol/openid-connect/token"
+
+  const [phoneNumber, setPhoneNumnber] = useState('123456789');
+  const [token, setToken] = useState('');
+  const [formattedValue, setFormattedValue] = useState('381123456789');
+  const [coverageResult, setCoverageResult] = useState(false);
+  const [authorizationResult, setAuthorizationResult] = useState();
+  const [disabled, setDisabled] = useState(false);
+  const [showMessage, setShowMessage] = useState(false);
+  const phoneInput = useRef<PhoneInput>(null);
+  
+
+ useEffect(() => {
+   
+   return () => {
+    if (Platform.OS === 'android') {
+      console.log("componentWillUnmount android")
+      RNCoverageService.unregisterNetwork();
+    }
    }
- };
+ }, [])
  checkCoverage = () => {
    console.log('checkCoverage');
    RNCoverageService.setAuthorizationServiceConfiguration("ipification-services.json")
@@ -31,60 +52,169 @@ export default class App extends Component {
      (error, isAvailable) => {
        console.log(' isAvailable ',isAvailable,  error);
        if (isAvailable) {
-         this.setState({activated: isAvailable});
+         setCoverageResult(isAvailable);
+         doAuthentication()
        } else {
-         this.setState({activated: error});
+        setCoverageResult(isAvailable || error);
+        setDisabled(false)
        }
      }
    );
  };
 
  doAuthentication = () => {
-   console.log('doAuthentication');
+   var state = getRandomValues();
+   console.log('doAuthentication',state );
+   RNAuthenticationService.setAuthorizationServiceConfiguration("ipification-services.json")
    RNAuthenticationService.doAuthorization(
-     {login_hint: '85263480857', state: '0829332e-3c3f-46ff-8de3-221c'},
+     {login_hint: formattedValue.replace("+",""), state: state},
      (error, code) => {
        console.log(error, code);
        if (code != null) {
-         this.setState({authentication: code});
+         setAuthorizationResult(code)
+         doTokenExchange()
        }
        else{
-         this.setState({authentication: error});
+        setAuthorizationResult(error)
        }
+       setDisabled(false)
      }
    );
  };
- render() {
+ doTokenExchange = async () =>{
+   console.log(authorizationResult)
+  
+  var details = {
+    'client_id': 'your_client_id',
+    'grant_type': 'authorization_code',
+    'client_secret': 'your_secret_key',
+    'redirect_uri': 'your_redirect_uri', 
+    'code': authorizationResult
+
+ };
+  var formBody = [];
+  for (var property in details) {
+    var encodedKey = encodeURIComponent(property);
+    var encodedValue = encodeURIComponent(details[property]);
+    formBody.push(encodedKey + "=" + encodedValue);
+  }
+  formBody = formBody.join("&");
+  console.log(formBody)
+  fetch(TOKEN_URL, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: formBody,
+  }).then((response) => response.json())
+  .then((responseJson) => {
+    console.log(responseJson)
+    console.log("access_token", responseJson["access_token"])
+      
+      var decoded = jwt_decode(responseJson["access_token"]);
+      // console.log(decoded)
+      setToken(JSON.stringify(decoded))
+  })
+  .catch((error) => {
+    console.error(error);
+  });;
+ }
+  getRandomValues = () =>{
+    const validChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var result = '';
+      for ( var i = 0; i < 40; i++ ) {
+          result += validChars.charAt(Math.floor(Math.random() * validChars.length));
+      }
+      return result;
+  }
    return (
-     <View style={styles.container}>
-       <Text style={{fontSize: 24}}>
-         CoverageApi Result: {`${this.state.activated}`}
-       </Text>
-       <Text style={{fontSize: 24, margin: 10}}>
-         DoAuthentcation Result:{' '}
-         <Text style={{fontSize: 11}}> {`${this.state.authentication}`}</Text>
-       </Text>
-       <View style={{height: 30}} />
-       <Button
-         onPress={this.checkCoverage}
-         title="Check Coverage "
-         color="green"
-       />
-       <View style={{height: 10}} />
-       <Button
-         onPress={this.doAuthentication}
-         title="Do Authentication"
-         color="blue"
-       />
-     </View>
+    <View style={styles.container}>
+    <SafeAreaView style={styles.wrapper}>
+      {showMessage && (
+        <View style={styles.message}>
+          <Text>Formatted Phone Number : {formattedValue}</Text>
+          <Text>Supported Telco : {coverageResult == true ? "true" : coverageResult == false ? "false" : coverageResult}</Text>
+          <Text>Authentication Result : {authorizationResult}</Text>
+          <Text>Access Token : {token}</Text>
+          
+        </View>
+      )}
+      <PhoneInput
+        ref={phoneInput}
+        defaultValue={phoneNumber}
+        defaultCode="RS"
+        layout="first"
+        onChangeText={(text) => {
+          setPhoneNumnber(text);
+        }}
+        onChangeFormattedText={(text) => {
+          setFormattedValue(text);
+          // setCountryCode(phoneInput.current?.getCountryCode() || '');
+        }}
+        countryPickerProps={{withAlphaFilter:true}}
+        disabled={disabled}
+        withDarkTheme
+        withShadow
+        autoFocus
+      />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          const checkValid = phoneInput.current?.isValidNumber(phoneNumber);
+          console.log(checkValid)
+          // setDisabled(true)
+          setShowMessage(true);
+          checkCoverage()
+        }}>
+        <Text style={styles.buttonText}>Authorize</Text>
+      </TouchableOpacity>
+      
+    </SafeAreaView>
+  </View>
    );
  }
-}
+
+
 const styles = StyleSheet.create({
- container: {
-   flex: 1,
-   justifyContent: 'center',
-   alignItems: 'center',
-   backgroundColor: '#d3d3d3',
- },
+  container: {
+    flex: 1,
+    backgroundColor: Colors.lighter,
+  },
+  wrapper: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    marginTop: 20,
+    height: 50,
+    width: 300,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#7CDB8A',
+    shadowColor: 'rgba(0,0,0,0.4)',
+    shadowOffset: {
+      width: 1,
+      height: 5,
+    },
+    shadowOpacity: 0.34,
+    shadowRadius: 6.27,
+    elevation: 10,
+  },
+  buttonText:{
+    color: 'white',
+    fontSize: 14,
+  },
+  redColor: {
+    backgroundColor: '#F57777'
+  },
+  message: {
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 20,
+    marginBottom: 20,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+  },
 });
+
+
+export default App;
