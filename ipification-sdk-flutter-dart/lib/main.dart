b2984a562a371c44d4d7524acb712911dd62a5a1
tinhnguyen <tn@ipification>
2021-03-01 19:21:12 +0700

    release flutter app

--- ipification-sdk-flutter-dart/lib/main.dart
+++ ipification-sdk-flutter-dart/lib/main.dart
@@ -1,11 +1,13 @@
-import 'dart:convert';
-import 'dart:io';
-import 'package:flutter/material.dart';
 import 'dart:async';
-import 'package:flutter/services.dart';
-import 'package:ip_sdk/ip_sdk.dart';
-import 'package:http/http.dart' as http;
+import 'dart:convert';
 import "dart:developer";
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+import 'package:intl_phone_number_input/intl_phone_number_input.dart';
+import 'package:ip_sdk/ip_sdk.dart';
 import 'package:jwt_decoder/jwt_decoder.dart';
 
 void main() {
@@ -20,7 +22,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   String authenCode = '';
   String alertMessage = '';
-  bool coverageAvailable = false;
+  bool isAvailable = false;
   final String TOKEN_URL =
       "https://stage.ipification.com/auth/realms/ipification/protocol/openid-connect/token";
 
@@ -28,6 +30,11 @@ class _MyAppState extends State<MyApp> {
 
   final String countryCode = "381";
   final String phoneNumber = "123456789";
+  String _phoneNum = "381123456789";
+
+  final TextEditingController controller = TextEditingController();
+  String initialCountry = 'RS';
+  PhoneNumber number = PhoneNumber(isoCode: 'RS', phoneNumber: "123456789");
 
   @override
   Widget build(BuildContext context) {
@@ -38,13 +45,50 @@ class _MyAppState extends State<MyApp> {
       body: ConstrainedBox(
           constraints: const BoxConstraints.expand(),
           child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
-                Text('$alertMessage\n', textAlign: TextAlign.center),
-                ElevatedButton(
-                  child: const Text('Authenticate'),
-                  onPressed: doAuthentication,
-                )
+                Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(height: 50),
+                        InternationalPhoneNumberInput(
+                          onInputChanged: (PhoneNumber number) {
+                            setState(() {
+                              _phoneNum = number.phoneNumber;
+                            });
+                          },
+                          hintText: "123456789",
+                          onInputValidated: (bool value) {
+                            // print(value);
+                          },
+                          selectorConfig: SelectorConfig(
+                            selectorType: PhoneInputSelectorType.BOTTOM_SHEET,
+                          ),
+                          ignoreBlank: true,
+                          autoValidateMode: AutovalidateMode.disabled,
+                          selectorTextStyle: TextStyle(color: Colors.black),
+                          initialValue: number,
+                          textFieldController: controller,
+                          formatInput: true,
+                          keyboardType: TextInputType.numberWithOptions(
+                              signed: true, decimal: true),
+                          inputBorder: OutlineInputBorder(),
+                          onSaved: (PhoneNumber number) {
+                            // print('On Saved: $number');
+                          },
+                        ),
+                        SizedBox(height: 30),
+                        ElevatedButton(
+                          child: const Text('Authenticate'),
+                          onPressed: startFlow,
+                        ),
+                        SizedBox(height: 30),
+                        Text('Result: $alertMessage\n',
+                            textAlign: TextAlign.center),
+                      ],
+                    ))
               ])),
     );
   }
@@ -57,61 +101,41 @@ class _MyAppState extends State<MyApp> {
     super.deactivate();
   }
 
-  Future<void> doAuthentication() async {
+  void doAuthorization() async {
     String errMessage;
     try {
-      setState(() {
-        alertMessage = "Checking Coverage";
-      });
-      if (Platform.isAndroid) {
-        IpSdk.setAuthorizationServiceConfiguration("ipification_services");
+      // if (Platform.isAndroid) {
+      //   IpSdk.setAuthorizationServiceConfiguration("ipification_services");
+      // }
+      if (_phoneNum.isEmpty) {
+        showMessage("please input your phone number");
+        return;
       }
+      _phoneNum = _phoneNum.replaceAll("+", "");
 
-      coverageAvailable = await IpSdk.checkCoverage;
-      print(coverageAvailable);
-    } on PlatformException catch (e) {
-      coverageAvailable = false;
-      errMessage = e.code + "\n" + e.message;
-    }
-    if (coverageAvailable == true || coverageAvailable == false) {
-      try {
-        if (Platform.isAndroid) {
-          IpSdk.setAuthorizationServiceConfiguration("ipification_services");
-        }
-        print(countryCode + phoneNumber);
-        authenCode =
-            await IpSdk.doAuthentication(loginHint: countryCode + phoneNumber);
-        if (authenCode.isNotEmpty) {
-          await doTokenExchange(
-              authenCode,
-              (success) => {
-                    errMessage =
-                        "Supported Telco : $coverageAvailable" + "\n\n",
-                    errMessage = errMessage +
-                        "Phone Number verified: ${success['phone_number_verified']}" +
-                        "\n\n",
-                    errMessage = errMessage +
-                        "Authentication Result: $authenCode" +
-                        "\n\n",
-                    errMessage =
-                        errMessage + "Access token: ${success['access_token']}"
-                  },
-              (fail) => {errMessage = fail});
-        }
-      } on PlatformException catch (e) {
-        errMessage = e.code + "\n" + e.message;
-      }
-    }
-
-    if (!mounted) return;
-
-    setState(() {
+      showMessage("call authentication service");
+      print(_phoneNum);
+      authenCode = await IpSdk.doAuthentication(loginHint: _phoneNum);
+      showMessage("authenCode $authenCode");
       if (authenCode.isNotEmpty) {
-        alertMessage = errMessage;
-      } else {
-        alertMessage = errMessage ?? 'Error: Coverage : unavailable';
+        var successMessage = "";
+        await doTokenExchange(
+            authenCode,
+            (success) => {
+                  successMessage =
+                      "got accesstoken with Phone Number verified: ${success['phone_number_verified']}" +
+                          "\n\n" +
+                          "MobileId: ${success['mobile_id']}" +
+                          "\n\n" +
+                          "Sub: ${success['sub']}",
+                  showMessage(successMessage)
+                },
+            (fail) => {errMessage = fail, showMessage(errMessage)});
       }
-    });
+    } on PlatformException catch (e) {
+      errMessage = e.code + "\n" + e.message;
+      showMessage(errMessage);
+    }
   }
 
   Future<void> doTokenExchange(var authentCode,
@@ -148,4 +172,39 @@ class _MyAppState extends State<MyApp> {
   }
 
   void nextPage() {}
+
+  void showMessage(String message) {
+    if (!mounted) return;
+    setState(() {
+      alertMessage = message;
+    });
+  }
+
+  Future<void> startFlow() async {
+   FocusScope.of(context).requestFocus(new FocusNode());
+
+
+    String errMessage;
+    try {
+      setState(() {
+        alertMessage = "Checking Coverage";
+      });
+      // if (Platform.isAndroid) {
+      //   IpSdk.setAuthorizationServiceConfiguration("ipification_services");
+      // }
+
+      isAvailable = await IpSdk.checkCoverage;
+      print("isAvailable $isAvailable");
+      showMessage("supported network: $isAvailable");
+    } on PlatformException catch (e) {
+      isAvailable = false;
+      errMessage = e.code + "\n" + e.message;
+    }
+    if (isAvailable == true) {
+      doAuthorization();
+    } else {
+      errMessage = "your Telco is not supported IPification";
+      showMessage(errMessage);
+    }
+  }
 }
