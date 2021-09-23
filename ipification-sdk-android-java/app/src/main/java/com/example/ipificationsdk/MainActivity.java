bcd89165b1db20f7f77b2158d5333f023b7ecd2d
tinhnguyen <tn@ipification>
2021-09-23 18:29:46 +0700

    correct setState

--- ipification-sdk-android-java/app/src/main/java/com/example/ipificationsdk/MainActivity.java
+++ ipification-sdk-android-java/app/src/main/java/com/example/ipificationsdk/MainActivity.java
@@ -182,8 +182,8 @@ public class MainActivity extends AppCompatActivity {
         };
         AuthRequest.Builder authRequestBuilder = new AuthRequest.Builder();
         authRequestBuilder.setScope("openid");
+//        authRequestBuilder.setState("your_state"); // generate state https://auth0.com/docs/protocols/state-parameters
         authRequestBuilder.addQueryParam("login_hint", phoneInputText.getText().toString());
-//        authRequestBuilder.addQueryParam("state", "213e23423423423423423423"); // generate state https://auth0.com/docs/protocols/state-parameters
         authService.performAuth(authRequestBuilder.build(), callback);
     }
 
