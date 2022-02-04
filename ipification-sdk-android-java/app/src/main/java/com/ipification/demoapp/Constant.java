693b4364e490bfa1cfcd161b4e5640312ec77ed9
nguyenhuutinh <nguyenhuutinh@users.noreply.github.com>
2022-02-04 11:12:04 +0700

    Update Constant.java

--- ipification-sdk-android-java/app/src/main/java/com/ipification/demoapp/Constant.java
+++ ipification-sdk-android-java/app/src/main/java/com/ipification/demoapp/Constant.java
@@ -1,7 +1,7 @@
 package com.ipification.demoapp;
 
 public  class  Constant {
-    public static String CLIENT_SECRET = "your_client_secret";
     public static String TOKEN_URL = "https://stage.ipification.com/auth/realms/ipification/protocol/openid-connect/token";
-    public static String DEVICE_TOKEN_REGISTRATION_URL = "your_registration_endpoint";
+    public static String DEVICE_TOKEN_REGISTRATION_URL = "https://device-token-url.com";
+    public static String CLIENT_SECRET = "your_client_secret";
 }
