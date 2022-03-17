a920e1c12a8fcc38248cf6c1562c8cf1349ce4d9
Jack N <nguyenhuutinh88@gmail.com>
2022-03-17 12:19:08 +0700

    update icon

--- ipification-sdk-ionic-angular/src/app/providers/constant.ts
+++ ipification-sdk-ionic-angular/src/app/providers/constant.ts
@@ -1,5 +1,5 @@
 export class Constant {
   static readonly TokenUrl: string = "https://stage.ipification.com/auth/realms/ipification/protocol/openid-connect/token";
-  static readonly ClientSecret: string = "4bc14abb-fd00-4fd7-b274-88205f2f11cb";
+  static readonly ClientSecret: string = "your-client-secret";
 
 }
