588ed9fe0826c51922f3ab5155ed6baa0b1adc14
T Nguyen <nguyenhuutinh@users.noreply.github.com>
2022-10-13 09:10:27 +0700

    Update SnippetCode.kt

--- ipification-sdk-android-kotlin/app/src/test/java/com/ipification/demoapp/SnippetCode.kt
+++ ipification-sdk-android-kotlin/app/src/test/java/com/ipification/demoapp/SnippetCode.kt
@@ -10,22 +10,26 @@ class PhoneVerifyActivity : AppCompatActivity() {
         updateButton(isEnable = false) // make sure user cannot double click to send multi requests at the same time
 
         val phoneNumber = "${binding.countryCodeEditText.text}${binding.phoneCodeEditText.text}"
-
+        
+        // 1. call checkCoverage API
         IPificationServices.startCheckCoverage(
             phoneNumber,
             context = this,
             callback = object : CellularCallback<CoverageResponse> {
                 override fun onError(error: CellularException) {
                     updateButton(isEnable = true)
+                    // error -> handle with SMS 
                     handleIPError("CheckCoverage Error: " + error.getErrorMessage())
                 }
 
                 override fun onSuccess(response: CoverageResponse) {
                     if (response.isAvailable()) {
+                        // 2. call Auth API
                         doIPAuth()
                     } else {
-                        handleIPError("CheckCoverage return false")
                         updateButton(isEnable = true)
+                        // unsupported -> handle with SMS 
+                        handleIPError("CheckCoverage return false")  
                     }
                 }
             })
@@ -47,11 +51,13 @@ class PhoneVerifyActivity : AppCompatActivity() {
                     //check auth_code
                     val code = response.getCode()
                     if (code != null) {
+                        // success -> call Token Exchange API
                         callTokenExchange(response.getCode()!!)
                         updateButton(isEnable = true)
                     } else {
                         binding.result.post {
                             updateButton(isEnable = true)
+                            // error -> handle with SMS 
                             handleIPError(response.responseData)
                         }
                     }
@@ -59,6 +65,7 @@ class PhoneVerifyActivity : AppCompatActivity() {
                 override fun onError(error: IPificationError) {
                     binding.result.post {
                         updateButton(isEnable = true)
+                        // error -> handle with SMS 
                         handleIPError(error.getErrorMessage())
                     }
                 }
@@ -66,6 +73,7 @@ class PhoneVerifyActivity : AppCompatActivity() {
     }
 
     private fun callTokenExchange(code: String) {
+        // 3. call your backend API to do token exchange
         APIManager.doPostToken(code, callback = object : TokenCallback {
             override fun onSuccess(response: String) {
                 handleTokenExchangeSuccess(response)
