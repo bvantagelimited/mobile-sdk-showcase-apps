c1cc891c63586dc001bc8ecb24dada47d882e462
Nguyen Huu Tinh <nguyenhuutinh@Tinh-Nguyens-MacBook-Pro.local>
2024-02-09 11:21:28 +0700

    Update IMAuthManualActivity.java

--- ipification-sdk-android-java/app/src/main/java/com/ipification/demoapp/activity/im/IMAuthManualActivity.java
+++ ipification-sdk-android-java/app/src/main/java/com/ipification/demoapp/activity/im/IMAuthManualActivity.java
@@ -45,6 +45,8 @@ public class IMAuthManualActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         initIPification();
         initView();
+
+        //FCM
         initFirebase();
 
     }
