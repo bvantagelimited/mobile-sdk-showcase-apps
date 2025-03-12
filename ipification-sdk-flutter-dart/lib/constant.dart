b2c53e569c098e57da68a45eca6d2d0f1b79b5f2
TN <nguyenhuutinh@users.noreply.github.com>
2025-03-12 14:13:13 +0700

    Update constant.dart

--- ipification-sdk-flutter-dart/lib/constant.dart
+++ ipification-sdk-flutter-dart/lib/constant.dart
@@ -3,20 +3,18 @@ class Constant {
   static const String _host = "https://api.stage.ipification.com";
 
   // API Endpoints.
-  static const String checkCoverageUrl = "$_host/auth/realms/ipification/coverage/";
-  static const String authUrl =
-      "$_host/auth/realms/ipification/protocol/openid-connect/auth";
   static const String tokenUrl =
       "$_host/auth/realms/ipification/protocol/openid-connect/token";
   static const String userInfoUrl =
       "$_host/auth/realms/ipification/protocol/openid-connect/userinfo";
 
   // Credentials and redirect URI.
+  // TODO: update with your value.
   static const String clientId = "";
   static const String clientSecret = "";
   static const String redirectUri = "";
 
-  // Device registration endpoint.
+  // IM AUTH - Device registration endpoint.
   // TODO: update with your value.
   static const String registerDeviceUrl = "";
 }
